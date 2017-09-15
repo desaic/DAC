688940ed8ff36bacfffc7c196034290ec9651dbd
desaic <desaicwtf@gmail.com>
2017-09-15 17:08:25 -0400

    remove unused code

--- viewer/src/Replay.cpp
+++ viewer/src/Replay.cpp
@@ -196,75 +196,14 @@ void Replay::run()
 {
   frame = frame0;
   int cnt = 0;
-  //std::ofstream out("angle.txt");
-  //double Energy = 0;
-  //Energy = em[0]->getAllEnergy();
-  //std::cout << "E0 " << Energy << "\n";
   int objCnt = 0;
   Eigen::Vector3d mn, mx;
-  
-  //std::string embedFile = "D:/workspace/GitHub/DesignDynamics/models/stair/head_minion.obj";
-  //TrigMesh embedTm;
-  //FileUtilIn in(embedFile);
-  //embedTm.load(in.in);
-  //in.close();
 
   ElementMesh* em0 = em[0];
   
   int e0 = em0->e.size();
   
-  //Eigen::Vector3d v0 = em0->X[em0->e[e0]->at(0)];
-  //Eigen::Vector3d vcenter = em0->X[em0->e[e0]->at(5)];
-  ////bounding box size of embedding 2x2 elements.
-  //Eigen::Vector3d dx = (em0->X[em0->e[e0]->at(7)] - v0);
-  //Eigen::Vector3d ebox = 2 * dx;
-
-  //std::vector<int> eidx(embedTm.v.size(), 0);
-  //std::vector<Eigen::Vector3d> natCoord(embedTm.v.size(), Eigen::Vector3d::Zero());
-  
-  //BBox(embedTm.v, mn, mx);
-  //for (int i = 0; i < (int)embedTm.v.size(); i++){
-  //  embedTm.v[i][0] -= 0.5 * (mn[0] + mx[0]);
-  //  embedTm.v[i][1] -= mn[1];
-  //  embedTm.v[i][2] -= 0.5 * (mn[2] + mx[2]);
-
-  //  for (int j = 0; j < 3; j++){
-  //    embedTm.v[i][j] *= ebox[j] / (mx[j] - mn[j]);
-  //  }
-  //  embedTm.v[i] += vcenter;
-  //  int gridIdx[3];
-  //  for (int j = 0; j < 3; j++){
-  //    gridIdx[j] = (int)( (embedTm.v[i][j] - v0[j])/ dx[j] );
-  //    if (gridIdx[j]>=2){
-  //      //std::cout << i << " " << embedTm.v[i] <<"\n"<<v0<< "\n";
-  //      gridIdx[j] = 1;
-  //    }
-  //    if (gridIdx[j] < 0){
-  //      gridIdx[j] = 0;
-  //    }
-  //  }
-  //  int ei = 4 * gridIdx[0] + 2 * gridIdx[1] + gridIdx[2] + e0;
-  //  eidx[i] = ei;
-  //  Element * ele = em0->e[ei];
-  //  Eigen::Vector3d ecenter = 0.5 * (em0->X[ ele->at(7)] + em0->X[ele->at(0)]);
-  //  for (int j = 0; j < 3; j++){
-  //    natCoord[i][j] = 2 * (embedTm.v[i][j] - ecenter[j]) / dx[j];
-  //  }
-  //}
-  //std::cout << viewStress << "\n";
-
-  //track 3 vertices
-  const int N_TRACK = 4;
-  int trackIdx[N_TRACK] = { 0, 0, 0, 0 };
   BBox(em[0]->X, mn, mx);
-  //trackIdx[0] = leftTop(em[0]->X, mn, mx);
-  Eigen::Vector3d targetX(0.048966, 0.068037, 0.0350494);
-  //trackIdx[0] = topRight(em[0]->X, mn, mx);
-  trackIdx[0] = closest(em[0]->X, targetX);
-  std::cout << "track 0 " << trackIdx[0] << "\n";
-  trackIdx[1] = backTop(em[0]->X, mn, mx);
-  trackIdx[2] = topOrigin(em[0]->X, mn, mx);
-  trackIdx[3] = rightBottom(em[0]->X, mn, mx);
   if (saveObj){
     for (int mi = 0; mi < (int)em.size(); mi++){
       for (int mat = 0; mat < 2; mat++){
@@ -295,33 +234,13 @@ void Replay::run()
       Eigen::Vector3d v = em[mi]->linearVelocity();
       double E = em[mi]->getAllEnergy();
       double ang = em[mi]->getTopAng();
-      //std::cout << "ang " << ang << "\n";
       std::cout << "E " << E << "\n";
       std::cout << "p " << p[0] <<" " <<p[1] <<" " <<p[2] << "\n";
       std::cout << "L " << L[0] << " " << L[1] << " " << L[2] << "\n";
       std::cout << "v " << v[0] << " " << v[1] << " " << v[2] << "\n";
       std::cout << "Height " << center[1] << "\n";
-      //Eigen::Vector3d tracku = em[mi]->x[trackIdx] - em[mi]->X[trackIdx];
-      //std::cout << "track x " << tracku << "\n";
-      //std::cout << "nu " << tracku[0]/ (1e-3 * frame) << "\n";
-      //out << frame*5e-5 << " ";
-      //for (int ti = 0; ti < 1; ti++){
-      //  for (int j = 0; j < 3; j++){
-      //    out << em[0]->x[trackIdx[ti]][j] << " ";
-      //  }
-      //}
-      //out << "\n";
 
-      Eigen::Vector3d mn, mx;
-      BBox(em[mi]->x, mn, mx);
-      std::cout << "nu " << -(1 - mx[0] + mn[0]) / (1 - mx[1] + mn[1]) << "\n";
-
-      //out << center[0] << " " << center[1] << " " << center[2] << " " 
-      //  << v[0] << " " << v[1] << " " << v[2] << " " 
-      //  << ang << " " << L[2] << " "<<E<<"\n";
-      //out << v[1] << "\n";
-      //std::cout << "center " << center[0] << " " << center[1] << "\n";
-      if (status >= 0){
+	  if (status >= 0){
         loadMore = true;
         frame = status;
         if (viewStress){
@@ -338,7 +257,6 @@ void Replay::run()
             }
           }
           std::cout << maxE << " " << maxStress << "\n";
-          //out << maxE << " " << maxStress << "\n";
         }
         
         updateSurfVert(em[mi]->x, trigm[mi], trigm[mi]->vidx);
@@ -351,12 +269,6 @@ void Replay::run()
       pauseSim();
     }
     
-    //Energy = em[0]->getEnergy(false);
-    //std::cout << "E: " << Energy << "\n";
-    //if (frame % 10 == 0){
-      //captureScreen = true;
-    //}
-
     if (saveObj){ 
       for (int mi = 0; mi < (int)em.size(); mi++){
         for (int mat = 0; mat < 2; mat++){
@@ -364,13 +276,7 @@ void Replay::run()
           objName << objDir << "/" << std::to_string(mi) << "m" <<mat<<"_"<< std::setw(4) << std::setfill('0') << std::to_string(objCnt) << ".obj";
           savePartObj(em[mi], mat, objName.str());
         }
-        //std::string objFile = objDir + "/" + std::to_string(mi) + "m" + std::to_string(objCnt) + ".obj";
-        //trigm[mi]->save_obj(objName.str().c_str());
       }
-      //std::stringstream objName;
-      //objName << objDir << "/" << "embed" << "_" << std::setw(4) << std::setfill('0') << std::to_string(objCnt) << ".obj";
-      //deformEmbed(em0, embedTm.v, eidx, natCoord);
-      //embedTm.save(objName.str().c_str());
       objCnt++;
     }
     cnt++;
@@ -391,7 +297,6 @@ void Replay::run()
     }
   }
 
-  //out.close();
 }
 
 void Replay::stepBackSim()
